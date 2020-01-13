175d2813f2efd073971bbe4218f5d0260b100caa
vk <vkhlous@gmail.com>
2020-01-13 20:02:19 +0200

    commit

--- main.c
+++ main.c
@@ -190,10 +190,10 @@ void run()
 
     }
 
-    printf("First name : %s\n", players[0].segvards);
+    //printf("First name : %s\n", players[0].segvards);
 
-   // set_games_countdown();
-    GAME_IN_PROCESS = 1;
+    set_games_countdown();
+    //GAME_IN_PROCESS = 1;
 
     while (GAME_IN_PROCESS == 0)
     {
@@ -264,7 +264,9 @@ void send_GAME_IN_PROCESS_MSG(connection_t * connection)
     char* msg_buffer = NULL;
 
     msg_buffer = (char *)malloc(MSG_GAME_IN_PROCESS_SIZE + 1);
-    snprintf(msg_buffer, MSG_GAME_IN_PROCESS_SIZE, MSG_GAME_IN_PROCESS); // TODO change
+    msg_buffer[0] = MSG_GAME_IN_PROCESS;
+    msg_buffer[1] = '\0';
+   // snprintf(msg_buffer, MSG_GAME_IN_PROCESS_SIZE, MSG_GAME_IN_PROCESS); // TODO change
     send_MSG(connection, msg_buffer);
 
     free(msg_buffer);
@@ -281,7 +283,7 @@ void send_MSG(connection_t * connection, char * msg_buffer)
     len = strlen(msg_buffer);
     msg_buffer[len] = '\0';
     send(connection->sock_desc, &len, sizeof(int), 0);
-    send(connection->sock_desc, msg_buffer, sizeof(msg_buffer), 0);
+    send(connection->sock_desc, msg_buffer, strlen(msg_buffer)+1, 0);
 }
 
 void send_MSG_TO_ALL_ACTIVE(char * msg_buffer)
@@ -315,6 +317,7 @@ void set_games_countdown()
         }
         else
         {
+            COUNTDOWN_STARTED = 1;
             pthread_cancel(countdown_thread);
             pthread_create(&countdown_thread, 0, start_countdown, NULL);
             pthread_detach(countdown_thread);
@@ -387,13 +390,9 @@ void start_game()
     // Send map to each
     read_send_game_MAP();
 
-    printf("Gamemap: %c\n", game_map[1][0]);
-
-    printf("Game map: %d\n", game_map[30][120]);
-
     print_game_map();
     set_players_coordinats();
-    print_game_map();
+  //  print_game_map();
 
     // Send positions to each
     send_game_update();
@@ -510,17 +509,17 @@ void add_MAP_line_to_array(char *map_line, int line_nr)
 {
     int i = 0;
     int j = 0;
-    for (; i < MAP_W-1; i++)
+    for (; i < MAP_W; i++)
     {
       //  printf("Put char %d into array cell {%d}{%d}\n", map_line[i], line_nr, i);
         game_map[line_nr][i] = map_line[i];
     }
 
-    printf("{%d}", line_nr);
-    for (; j < MAP_W; j ++)
-    {
-        printf("%c", game_map[line_nr][j]);
-    }
+//    printf("{%d}", line_nr);
+//    for (; j < MAP_W; j ++)
+//    {
+//        printf("%c", game_map[line_nr][j]);
+//    }
 
    // printf("Last i:%d\n", i);
    // printf("Last char for line nr - %d: %d\n", line_nr,  game_map[line_nr][MAP_W-2]); // MAP_W - 1, cuz starting from 0.
@@ -551,7 +550,7 @@ void send_MAP_ROW_MSG(char *line, int line_nr)
     msg_buffer = (char *)malloc(MSG_MAP_ROW_SIZE + 1);
     //6<rindas_nummurs><kartes_rinda>
     snprintf(msg_buffer, MSG_MAP_ROW_SIZE, MSG_MAP_ROW, line_nr, line);
-    printf("Message to send: %s\n",msg_buffer);
+    //printf("Message to send: %s\n",msg_buffer);
 
    // send_MSG(conn, msg_buffer);
 
@@ -570,11 +569,11 @@ void send_GAME_START_MSG()
   //  conn = players[client_nr].connection;
     //conn = (connection_t *)client -> connection;
 
-    msg_buffer = (char *)malloc(MSG_GAME_START_SIZE + (client_count * Ns_PLAYER_NAME_SIZE));
-    list_of_players = (char *)malloc(client_count * Ns_PLAYER_NAME_SIZE);
+    msg_buffer = (char *)malloc(500); // TODO!
+    list_of_players = (char *)malloc(500);
 
     int i = 0;
-    char temp[Ns_PLAYER_NAME_SIZE];
+    char temp[Ns_PLAYER_NAME_SIZE+1];
     for (; i < client_count - 1; i++)
     {
         sprintf(temp, Ns_PLAYER_NAME, players[i].segvards);
@@ -583,6 +582,8 @@ void send_GAME_START_MSG()
 
     sprintf(temp, "<%s>", players[client_count - 1].segvards);
     strcat(list_of_players, temp);
+
+    printf("LIST OF PLAYERS:%s\n", list_of_players);
 
    // 5<spēlētāju_skaits>{<ntā_spēlētāja_segvārds>}<kartes_platums><kartes_augstums>
     sprintf(msg_buffer, MSG_GAME_START, client_count, list_of_players, MAP_W, MAP_H);
@@ -604,7 +605,18 @@ void send_GAME_START_MSG()
 
 void * start_countdown(void * ptr)
 {
-    
+    int i;
+
+    for(; i < 10; i++)
+    {
+        printf("Countdown:%d\n", i);
+        sleep(1);
+    }
+
+    GAME_IN_PROCESS = 1;
+
+    printf("\n");
+    pthread_exit(0);
 
 }
 
@@ -655,8 +667,10 @@ void send_USERNAME_TAKEN_MSG(connection_t * connection)
     int len;
     char* msg_buffer = NULL;
 
-    msg_buffer = (char *)malloc(MSG_USERNAME_TAKEN_SIZE + 1);
-    snprintf(msg_buffer, MSG_USERNAME_TAKEN_SIZE, MSG_USERNAME_TAKEN); // TODO change
+    msg_buffer = (char *)malloc(2);
+    msg_buffer[0] = MSG_USERNAME_TAKEN;
+    msg_buffer[1] = '\0';
+    //snprintf(msg_buffer, MSG_USERNAME_TAKEN_SIZE, MSG_USERNAME_TAKEN); // TODO change
     send_MSG(connection, msg_buffer);
 
     free(msg_buffer);
@@ -737,14 +751,14 @@ void send_LOBBY_INFO_MSG()
 
 //    printf("connection read,\n");
 
-    msg_buffer = (char *)malloc(1000);
-    list_of_players = (char *)malloc(client_count * Ns_PLAYER_NAME_SIZE);
+    msg_buffer = (char *)malloc(500); //TODO
+    list_of_players = (char *)malloc(500);
 
     printf("malloc done\n");
 
     int i = 0;
-    char temp[Ns_PLAYER_NAME_SIZE];
-    for (; i < client_count - 1; i++)
+    char temp[Ns_PLAYER_NAME_SIZE+1];
+    for (; i < client_count; i++)
     {
         sprintf(temp, Ns_PLAYER_NAME, players[i].segvards);
         strcat(list_of_players, temp);
@@ -752,10 +766,12 @@ void send_LOBBY_INFO_MSG()
 
     printf("Players list done\n");
 
-    sprintf(temp, "<%s>", players[client_count - 1].segvards);
+    sprintf(temp, "<%s>", players[client_count].segvards);
     strcat(list_of_players, temp);
 
     printf("players list done done\n");
+
+    printf("LIST OF PLAYERS: %s\n", list_of_players);
 
     //"2<%d>{%s}"
    sprintf(msg_buffer, MSG_LOBBY_INFO, client_count + 1, list_of_players);
@@ -802,7 +818,7 @@ char * process_income_message(char *buffer) {
             to_return[j] = buffer[i];
             j++;
         }
-        to_return[SEGVARDS_SIZE] = '\0';
+        to_return[strlen(buffer)-1] = '\0';
     }
 
     printf("TO RETURN %s\n", to_return);
